1766515711168e6637953c9b721917f8aa0e56e6
kumaran-14 <kumarangowrisankar@yahoo.in>
2019-09-11 23:23:45 +0530

    Initial setup
    - Configure CMake files
    - Add googletest submodule
    - Add main.cpp in src/

---
+++ test/test_main.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+ using namespace std;
+
+ /**
+ * Tests to make sure tests are testing (testing tests)
+ */
+TEST(Test, TestingTest) { EXPECT_EQ(5, 5); }
+
+ /**
+ * Run Tests
+ */
+int main(int argc, char *argv[]) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
