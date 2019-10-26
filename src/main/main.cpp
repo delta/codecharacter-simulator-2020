f801548c21f86f7c025b6b3d5d000a6f54f6efb9
Kumaran <kumarangowrisankar@yahoo.in>
2019-10-26 21:53:15 +0530

    Setup CircleCI config

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-	std::cout<<"It works? It works!!!"<<std::endl;
-	return 0;
+  std::cout << "It works? It works!!!" << std::endl;
+  return 0;
 }
