f9e4d270d942234f763aaa80404e541f1ff34f4f
shb9019 <saihemanth9019@gmail.com>
2019-12-16 15:20:35 +0530

    Add IUpdatable and ICommandTaker interfaces (#5)

---
+++ src/state/include/state/interfaces/i_command_taker.h
@@ -0,0 +1,54 @@
+/**
+ * @file i_command_taker.h
+ * Interface for game state changing calls
+ */
+
+#pragma once
+
+#include "physics/vector.hpp"
+#include "state/interfaces/i_updatable.h"
+#include "state/state_export.h"
+#include "state/utilities.h"
+
+namespace state {
+
+/**
+ * ICommandTaker interface to define the game action methods
+ */
+class STATE_EXPORT ICommandTaker : public IUpdatable {
+public:
+  virtual ~ICommandTaker() {}
+
+  /**
+   * Handles bot movement
+   *
+   * @param[in]  player_id     Player to act upon
+   * @param[in]  bot_id    Bot to act upon
+   * @param[in]  position      New position to move the bot to
+   *
+   * @throw      std::exception  if the operation was not possible
+   */
+  virtual void MoveBot(PlayerId player_id, int64_t bot_id,
+                       physics::Vector<int64_t> position) = 0;
+
+  /**
+   * Handles bot transform
+   *
+   * @param[in]  player_id     Player to act upon
+   * @param[in]  bot_id    Bot to act upon
+   *
+   * @throw      std::exception  if the operation was not possible
+   */
+  virtual void TransformBot(PlayerId player_id, int64_t bot_id) = 0;
+
+  /**
+   * Blast a bot and destroy units nearby
+   *
+   * @param[in]  player_id     Player to act upon
+   * @param[in]  bot_id      Bot to act upon
+   *
+   * @throw      std::exception  if the operation was not possible
+   */
+  virtual void BlastBot(PlayerId player_id, int64_t bot_id) = 0;
+};
+} // namespace state
---
+++ src/state/include/state/state.h
@@ -0,0 +1,16 @@
+/**
+ * @file state.h
+ * Declarations for State which representing the internal state of the game
+ */
+
+#pragma once
+
+#include "physics/vector.hpp"
+#include "state/interfaces/i_command_taker.h"
+#include "state/interfaces/i_updatable.h"
+#include "state/utilities.h"
+
+namespace state {
+
+class STATE_EXPORT State {};
+} // namespace state
