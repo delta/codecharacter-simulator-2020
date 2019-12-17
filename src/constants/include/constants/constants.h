dd420f0e8e1abda521d8a9dcdd3d336f4c1c50c6
Sai Hemanth B <saihemanth9019@gmail.com>
2019-12-17 17:56:27 +0530

    Add constants module
    - Add basic state constants

---
+++ src/constants/include/constants/constants.h
@@ -0,0 +1,9 @@
+/**
+ * @file constants.h
+ * Aggregates all constants headers
+ */
+
+#pragma once
+
+#include "constants/actor.h"
+#include "constants/map.h"
---
+++ src/constants/include/constants/map.h
@@ -0,0 +1,25 @@
+/**
+ * @file map.h
+ * Constants related to map
+ */
+
+#pragma once
+
+#include "physics/vector.hpp"
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+// Size of the map
+const size_t MAP_SIZE = 100;
+
+// Position of home bases of players
+// Player 2 base position is flipped position of player 1
+const auto PLAYER1_BASE_POSITION = DoubleVec2D{5, 5};
+const auto PLAYER2_BASE_POSITION =
+        DoubleVec2D{(MAP_SIZE - 1) - PLAYER1_BASE_POSITION.x,
+                    (MAP_SIZE - 1) - PLAYER1_BASE_POSITION.y};
+
+// Base positions of both players
+const std::vector<DoubleVec2D> PLAYER_BASE_POSITIONS = {PLAYER1_BASE_POSITION,
+                                                        PLAYER2_BASE_POSITION};
