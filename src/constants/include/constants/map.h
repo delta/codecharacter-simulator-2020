0d1f261591ce991b9ecbe970c81f2491f66fea19
Sai Hemanth B <saihemanth9019@gmail.com>
2019-12-17 18:04:01 +0530

    Run clangformat

--- src/constants/include/constants/map.h
+++ src/constants/include/constants/map.h
@@ -17,8 +17,8 @@ const size_t MAP_SIZE = 100;
 // Player 2 base position is flipped position of player 1
 const auto PLAYER1_BASE_POSITION = DoubleVec2D{5, 5};
 const auto PLAYER2_BASE_POSITION =
-        DoubleVec2D{(MAP_SIZE - 1) - PLAYER1_BASE_POSITION.x,
-                    (MAP_SIZE - 1) - PLAYER1_BASE_POSITION.y};
+    DoubleVec2D{(MAP_SIZE - 1) - PLAYER1_BASE_POSITION.x,
+                (MAP_SIZE - 1) - PLAYER1_BASE_POSITION.y};
 
 // Base positions of both players
 const std::vector<DoubleVec2D> PLAYER_BASE_POSITIONS = {PLAYER1_BASE_POSITION,
