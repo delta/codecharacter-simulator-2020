e18bf73d48dde45c62349964314ea466549e54b7
shb9019 <saihemanth9019@gmail.com>
2019-12-18 15:16:32 +0530

    Add constants module (#7)

---
+++ src/constants/include/constants/actor.h
@@ -0,0 +1,26 @@
+/**
+ * @file actor.h
+ * Constants related to game actors
+ */
+
+#pragma once
+
+#include "physics/vector.hpp"
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+// Maximum number of bots per player
+const size_t MAX_NUM_BOTS = 500;
+
+// Number of bots that each player starts with
+const size_t NUM_BOTS_START = 20;
+
+// Blast impact radius around the bot (Manhattan distance)
+const size_t BLAST_IMPACT_RADIUS = 3;
+
+// Speed of a bot in manhattan distance per turn
+const int64_t BOT_SPEED = 2;
+
+// Frequency at which bots spawn at home base per turn
+const int64_t BOT_SPAWN_FREQUENCY = 1;
