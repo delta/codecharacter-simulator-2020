4f162f822f7d9e007e4743c0bb6d8cfe180eaced
bestin <bestinbthomas@gmail.com>
2019-12-27 18:28:33 +0530

    [ADD] cmake-format config and githook

---
+++ .githooks/cmakeformat.sh
@@ -0,0 +1,7 @@
+for file in $(git diff --name-only --staged); do
+        if [[ "$file" =~ .*(.cmake|CMakeLists.txt)$ ]];     #  this is the snag
+        then
+        echo formatting $file
+            cmake-format -i $file && git add $file
+        fi
+done;
