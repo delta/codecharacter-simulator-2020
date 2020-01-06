3bd8d3353a9e7827e406f8a07ed7d496c92d955e
Bestin <bestinbthomas@gmail.com>
2020-01-06 21:08:58 +0530

    fixup! fixup! fixup! [ADD] Cmake-format

--- .githooks/clangformat.sh
+++ .githooks/clangformat.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
+echo "## Running clang-format on all staged CXX source files ##"
 for file in $(git diff --name-only --staged); do
         if [[ "$file" =~ .*\.(hpp|cpp|c|h)$ ]];     #  this is the snag
         then
                 clang-format -i $file && git add $file
+                echo "Ran on ${file}"
         fi
 done;
 
--- .githooks/cmakeformat.sh
+++ .githooks/cmakeformat.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
+echo "## Runnig cmake-format on cmake lists files ##"
 for file in $(git diff --name-only --staged); do
         if [[ "$file" =~ .*(.cmake|CMakeLists.txt)$ ]];     #  this is the snag
         then
-            echo formatting $file
             cmake-format -i $file && git add $file
+            echo "Ran on ${file}"
         fi
 done;
