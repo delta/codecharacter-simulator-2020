b42e258d45d61fb1a56ba56d044c4d89b51afbd9
kumaran-14 <kumarangowrisankar@yahoo.in>
2020-01-07 20:54:45 +0530

    Add Actor, Unit, Bot, Tower definitions

--- src/state/include/state/player_state.h
+++ src/state/include/state/player_state.h
@@ -10,14 +10,13 @@
 #include "state/interfaces/i_updatable.h"
 #include "state/utilities.h"
 
-// #include <iostream>
 #include <array>
 
 namespace player_state {
 using TerrainType = state::TerrainType;
 
 enum class BotState : int8_t {
-  // Bot is idle, i.e doing  nothing
+  // Bot is doing  nothing
   IDLE,
   // Bot is moving
   MOVE,
@@ -63,24 +62,71 @@ std::ostream &operator<<(std::ostream &os, TowerState tower_state) {
 
 struct _Actor {
   int64_t id;
-	physics::Vector<int64_t> position;
-	int64_t hp;
+  int64_t hp;
+  Vec2D position;
+
+  _Actor() : id(0), hp(100), position(Vec2D{0, 0}){};
 };
 
-struct Bot : _Actor{
+struct _Unit : _Actor {
+  Vec2D destination;
+  static size_t speed;
 
+  void reset() { destination = Vec2D::null; }
+  void move(Vec2D p_destination) {
+    reset();
+    destination = p_destination;
+  }
+
+  _Unit() : _Actor(){};
 };
+
+size_t _Unit::speed = Constants::Actor::BOT_SPEED;
+
+struct Bot : _Unit {
+  BotState state;
+  static size_t impact_radius;
+
+  void blast_bot() {
+    reset();
+    //
+  }
+
+  void transform_bot() {
+    reset();
+    //
+  }
+
+  Bot() : _Unit(){};
+};
+
+size_t Bot::impact_radius = Constants::Actor::BLAST_IMPACT_RADIUS;
 
 std::ostream &operator<<(std::ostream &os, Bot bot) {
-  // complete this, based on Bot definition
+  using std::endl;
+  os << "Bot(id: " << bot.id << ") {" << endl;
+  os << "   hp: " << bot.hp << endl;
+  os << "   position: " << bot.position << endl;
+  os << "   state: " << bot.state << endl;
+  os << "}" << endl;
 }
 
-struct Tower : _Actor {};
+struct Tower : _Actor {
+  TowerState state;
+  void suicide_tower() {
+    //
+  }
+  Tower() : _Actor(){};
+};
 
 std::ostream &operator<<(std::ostream &os, Tower tower) {
-  // complete this, based on Tower defintion.
+  using std::endl;
+  os << "Tower(id: " << tower.id << ") {" << endl;
+  os << "   hp: " << tower.hp << endl;
+  os << "   position: " << tower.position << endl;
+  os << "   state: " << tower.state << endl;
+  os << "}" << endl;
 }
-
 
 struct MapElement {
   TerrainType type;
@@ -90,77 +136,82 @@ struct MapElement {
  * Main Player state, the struct interface available to each player.
  */
 struct State {
-  std::array<std::array<MapElement, Constants::Map::MAP_SIZE>, Constants::Map::MAP_SIZE> map;
+  std::array<std::array<MapElement, Constants::Map::MAP_SIZE>,
+             Constants::Map::MAP_SIZE>
+      map;
 
-  std::array<Bot, Constants::Actor::MAX_NUM_BOTS> bots;
-  std::array<Bot, Constants::Actor::MAX_NUM_BOTS> enemy_bots;
+  std::vector<Bot> bots;
+  std::vector<Bot> enemy_bots;
 
   int64_t num_bots;
   int64_t num_enemy_bots;
 
-  std::array<Tower, Constants::Map::MAP_SIZE*Constants::Map::MAP_SIZE> towers;
-  std::array<Tower, Constants::Map::MAP_SIZE*Constants::Map::MAP_SIZE> enemy_towers;
+  std::vector<Tower> towers;
+  std::vector<Tower> enemy_towers;
 
   int64_t num_towers;
   int64_t num_enemy_towers;
 
   int64_t score;
-  int64_t interestingness;
   int64_t interest_threshold;
+
+  State()
+      : bots(Constants::Actor::MAX_NUM_BOTS),
+        enemy_bots(Constants::Actor::MAX_NUM_BOTS),
+        num_bots(Constants::Actor::MAX_NUM_BOTS),
+        num_enemy_bots(Constants::Actor::MAX_NUM_BOTS),
+        towers(Constants::Map::MAP_SIZE * Constants::Map::MAP_SIZE),
+        enemy_towers(Constants::Map::MAP_SIZE * Constants::Map::MAP_SIZE),
+        num_towers(Constants::Map::MAP_SIZE * Constants::Map::MAP_SIZE),
+        num_enemy_towers(Constants::Map::MAP_SIZE * Constants::Map::MAP_SIZE),
+        score(0), interest_threshold(0) {}
 };
 
-
-
 std::ostream &operator<<(std::ostream &os, State state) {
-	using std::endl;
+  using std::endl;
 
-	os << "Map:" << endl;
-	for (auto const &row : state.map) {
-		for (auto const &elem : row) {
-			switch (elem.type) {
-			case TerrainType::LAND:
-				os << "L"
-				   << " ";
-				break;
-			case TerrainType::WATER:
-				os << "W"
-				   << " ";
-				break;
-			case TerrainType::FLAG:
-				os << "F"
-				   << " ";
-				break;
-			}
-		}
-		os << endl;
-	}
+  os << "Map:" << endl;
+  for (auto const &row : state.map) {
+    for (auto const &elem : row) {
+      switch (elem.type) {
+      case TerrainType::LAND:
+        os << "L ";
+        break;
+      case TerrainType::WATER:
+        os << "W ";
+        break;
+      case TerrainType::FLAG:
+        os << "F ";
+        break;
+      }
+    }
+    os << endl;
+  }
 
-	os << "-- Bots --" << endl;
-	for (auto const &bot : state.bots) {
-		os << bot << endl;
-	}
+  os << "-- Bots --" << endl;
+  for (auto const &bot : state.bots) {
+    os << bot << endl;
+  }
 
   os << "-- Enemy Bots --" << endl;
-	for (auto const &enemy_bot : state.enemy_bots) {
-		os << enemy_bot << endl;
-	}
+  for (auto const &enemy_bot : state.enemy_bots) {
+    os << enemy_bot << endl;
+  }
 
+  os << "-- Towers --" << endl;
+  for (auto const &tower : state.towers) {
+    os << tower << endl;
+  }
 
-	os << "-- Towers --" << endl;
-	for (auto const &tower : state.towers) {
-		os << tower << endl;
-	}
+  os << "-- Enemy Towers --" << endl;
+  for (auto const &enemy_tower : state.enemy_towers) {
+    os << enemy_tower << endl;
+  }
 
+  os << "-- Score --" << endl;
+  os << state.score << endl;
 
-	os << "-- Enemy Tower --" << endl;
-	for (auto const &enemy_tower : state.enemy_towers) {
-		os << enemy_tower << endl;
-	}
-
-	os << "-- Score --" << endl;
-	os << state.score << endl;
-
-	return os;
+  return os;
 }
 
 } // namespace player_state
