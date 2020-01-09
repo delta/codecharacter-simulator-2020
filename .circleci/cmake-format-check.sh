0eb7312042e4a328f438e5609e46aad963777c55
Bestin <bestinbthomas@gmail.com>
2020-01-09 20:57:00 +0530

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add cmake-format check in circleci

--- .circleci/cmake-format-check.sh
+++ .circleci/cmake-format-check.sh
@@ -2,19 +2,19 @@
 set -o nounset
 set -o pipefail
 
-# Run Clangformat
+# Run CMake-Format
 cd /root/project/build
-find . \( -name '*.cmake' -o -name 'CMakeLists.txt' \) -exec cmake-format -i {} \;   || { echo "Make Clangformat failed!"; exit 1; }
+find . \( -name '*.cmake' -o -name 'CMakeLists.txt' \) -exec cmake-format -i {} \;   || { echo "CMake-format run failed!"; exit 1; }
 
 # Check git for changes
 cd /root/project
-cmakeformatcheck=`git status | grep "modified"`
-if [[ -z $cmakeformatcheck ]]; then
+listcmakeformatcheck=`git status | grep "modified"`
+if [[ -z $listcmakeformatcheck ]]; then
     echo "Cmake-Format test passed!"
     exit 0;
 else
     echo "You have cmake-format problems :";
-    for f in $cmakeformatcheck; do
+    for f in $listcmakeformatcheck; do
         filename=`echo $f | awk '{print $2}'`
         echo $filename
         git diff $filename
