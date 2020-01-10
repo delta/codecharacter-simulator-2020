6bbe47f905315c279bbb4dda3d73503e778aaa0f
Bestin <bestinbthomas@gmail.com>
2020-01-10 22:39:11 +0530

    testing 2

--- FindCmakeFormat.cmake
+++ FindCmakeFormat.cmake
@@ -3,7 +3,7 @@
 string(REPLACE ":" ";" __PATH $ENV{PATH})
 
 foreach(p ${__PATH})
-  file(GLOB cmke ${p}/cmake-format)
+  file(GLOB cmke ${p}/cmake-format ${p}/cmake-format-[0-9]*)
   if(cmke)
     set(CMAKE_FORMAT_EXECUTABLE ${cmke})
     set(CMAKE_FORMAT_FOUND ON)
