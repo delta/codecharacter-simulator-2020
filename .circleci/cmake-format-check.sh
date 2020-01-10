3354a5cb64a3538de072f07bff5fda32dd13f5f8
Bestin B Thomas <49572140+bestinbthomas@users.noreply.github.com>
2020-01-10 23:19:59 +0530

    Update cmake-format-check.sh

--- .circleci/cmake-format-check.sh
+++ .circleci/cmake-format-check.sh
@@ -4,21 +4,21 @@ set -o pipefail
 
 # Run cmake-format
 cd /root/project
-find . \( -type f -not -path '*build*' -not -path '*ext*' -name '*.cmake' -o -name 'CMakeLists.txt' \)   -exec cmake-format --check {} \;   || { echo "cmake-format run failed!"; exit 1; }
+find . \( -type f -not -path '*build*' -not -path '*ext*' -name '*.cmake' -o -name 'CMakeLists.txt' \)   -exec cmake-format -i {} \;   || { echo "cmake-format run failed!"; exit 1; }
 
-# Check git for changes
-# cd /root/project
-# cmakeformatcheklist=`git status | grep "modified"`
-# echo $cmakeformatcheklist
-# if [[ -z $cmakeformatcheklist ]]; then
-#     echo "cmake-format test passed!"
-#     exit 0;
-# else
-#     echo "You have cmake-format problems :";
-#     for f in $cmakeformatcheklist; do
-#         filename=`echo $f | awk '{print $2}'`
-#         echo $filename
+Check git for changes
+cd /root/project
+cmakeformatcheklist=`git status | grep "modified"`
+echo $cmakeformatcheklist
+if [[ -z $cmakeformatcheklist ]]; then
+    echo "cmake-format test passed!"
+    exit 0;
+else
+    echo "You have cmake-format problems :";
+    for f in $cmakeformatcheklist; do
+        filename=`echo $f | awk '{print $2}'`
+        echo $filename
 #         git diff $filename --exit-code
-#     done
-#     exit 1;
-# fi
+    done
+    exit 1;
+fi
