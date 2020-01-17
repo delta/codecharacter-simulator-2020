5b439300c40a0d7e74eb6197e1eb3ffbd1dac266
Hrishikesh Shekhar <hrishikeshshekhar@gmail.com>
2020-01-17 19:45:53 +0530

    Add i_actor_state interface

---
+++ src/state/include/state/actor/interfaces/i_actor_state.h
@@ -0,0 +1,36 @@
+/**
+ * @file i_actor_state.h
+ * Declares an interface for actor states
+ */
+
+#include "state/state_export.h"
+#include <memory>
+
+#pragma once
+
+namespace state {
+
+class STATE_EXPORT IActorState {
+public:
+  virtual ~IActorState() {}
+
+  /**
+   * Called right after the actor switches to this state
+   */
+  virtual void Enter() = 0;
+
+  /**
+   * Executes state code when called
+   * Returns the next actor state when a state transition occurs
+   * Returns nullptr if no state transition occurs (no transition)
+   *
+   * @return      A pointer to the new state
+   */
+  virtual std::unique_ptr<IActorState> Update() = 0;
+
+  /**
+   * Called before the actor switches to another state
+   */
+  virtual void Exit() = 0;
+};
+} // namespace state
