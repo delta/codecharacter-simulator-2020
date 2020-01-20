1ca26ba8cdcefbed95612c20b4a230e62fff34ff
Hrishikesh Shekhar <hrishikeshshekhar@gmail.com>
2020-01-20 19:25:02 +0530

    Add state syncer headers
    - Add state syncer interface
    - Define state syncer class
    - Modify command taker interface

--- src/state/include/state/state.h
+++ src/state/include/state/state.h
@@ -8,9 +8,78 @@
 #include "physics/vector.hpp"
 #include "state/interfaces/i_command_taker.h"
 #include "state/interfaces/i_updatable.h"
+#include "state/map/map.h"
+#include "state/score_manager/score_manager.h"
 #include "state/utilities.h"
 
 namespace state {
 
-class STATE_EXPORT State {};
+class STATE_EXPORT State : public ICommandTaker {
+  private:
+    /**
+     * Map instance that maintains game terrain
+     */
+    std::unique_ptr<Map> map;
+
+    /**
+     * Score Manager instance to maintain player score
+     */
+    std::unique_ptr<ScoreManager> score_manager;
+
+    /**
+     * Function to check whether the opponent is also requesting to change to
+     * a tower at the same position
+     *
+     * @param[in] position		Position which is looked for in the
+     * opponents build requests
+     * @param[in] enemy_id		id of the enemy
+     *
+     */
+    bool isPositionOccupied(Vec2D position, size_t enemy_id);
+
+    /**
+     * Compute scores for this turn, and record them
+     */
+    void UpdateScores();
+
+  public:
+    /**
+     * Constructors
+     */
+    State();
+
+    State(std::unique_ptr<Map> map,
+          std::unique_ptr<ScoreManager> score_manager);
+
+    /**
+     * @see ICommandTaker#MoveBot
+     */
+    void moveBot(PlayerId player_id, ActorId actor_id,
+                 DoubleVec2D position) override;
+
+    /**
+     * @see ICommandTaker#TransformBot
+     */
+    void transformBot(PlayerId player_id, ActorId bot_id) override;
+
+    /**
+     * @see ICommandTaker#BlastActor
+     */
+    void blastActor(PlayerId player_id, ActorId actor_id) override;
+
+    /**
+     * @see ICommandTaker#GetMap
+     */
+    const std::unique_ptr<Map> getMap() const override;
+
+    /**
+     * @see ICommandTaker#GetScores
+     */
+    const std::array<int64_t, 2> getScores(bool game_over) const override;
+
+    /**
+     * @see ICommandTaker#IsGameOver
+     */
+    bool isGameOver(PlayerId &winner) override;
+};
 } // namespace state
---
+++ src/state/include/state/state_syncer.h
@@ -0,0 +1,78 @@
+/**
+ * @file state_syncer.h
+ * Declares the StateSyncer class
+ */
+
+#pragma once
+
+#include "logger/interfaces/i_logger.h"
+#include "state/interfaces/i_state_syncer.h"
+#include <memory>
+
+namespace state {
+
+class STATE_EXPORT StateSyncer : public IStateSyncer {
+  private:
+    /**
+     * Instance of the state for giving commands to
+     */
+    std::unique_ptr<ICommandGiver> command_giver;
+
+    /**
+     * Instance of command giver for giving commands to state
+     */
+    std::unique_ptr<ICommandTaker> state;
+
+    /**
+     * Instance of logger to log every turn and to log errors
+     */
+    std::unique_ptr<logger::ILogger> logger;
+
+  public:
+    StateSyncer();
+
+    StateSyncer(std::unique_ptr<ICommandTaker> state,
+                std::unique_ptr<ICommandGiver> command_giver,
+                std::unique_ptr<logger::ILogger> logger);
+
+    /**
+     * @see IStateSyncer #UpdateMainState
+     */
+    void updateMainState(std::array<player_state::State, 2> &player_states,
+                         std::array<bool, 2> skip_turns) override;
+
+    /**
+     * @see IStateSyncer #UpdatePlayerStates
+     */
+    void updatePlayerStates(std::array<player_state::State, 2> &player_states);
+
+    /**
+     * @see IStateSyncer #IsGameOver
+     */
+    bool isGameOver(PlayerId &winner);
+
+    /**
+     * @see IStateSyncer #GetScores
+     */
+    std::array<int64_t, 2> GetScores(bool game_over);
+
+    /**
+     *  Function to the assign player state bots their new states after
+     * validation of user's actions
+     */
+    void AssignBots(std::vector<player_state::Bot> player_bots, bool is_enemy);
+
+    /**
+     *  Function to the assign player state towers their new states after
+     * validation of user's actions
+     */
+    void AssignTowers(std::vector<player_state::Tower> player_towers,
+                      bool is_enemy);
+
+    /**
+     * Helper function to get the player id
+     */
+    int64_t GetPlayerId(int id, bool is_enemy);
+};
+
+} // namespace state
