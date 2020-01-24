60adf9ae29be98453975a565f0f8cc11edcd00d0
Bestin <bestinbthomas@gmail.com>
2020-01-24 23:20:11 +0530

    fixup! fixup! fixup! updated score manager

--- src/state/src/score_manager/score_manager.cpp
+++ src/state/src/score_manager/score_manager.cpp
@@ -18,12 +18,14 @@ void ScoreManager::botExitedFlagArea(PlayerId player_id) {
 }
 
 void ScoreManager::updateScore() {
-    if (no_of_bots[(int)PlayerId::PLAYER1] > no_of_bots[(int)PlayerId::PLAYER2])
-        scores[(int)PlayerId::PLAYER1]++;
-
-    else if (no_of_bots[(int)PlayerId::PLAYER2] >
-             no_of_bots[(int)PlayerId::PLAYER1])
-        scores[(int)PlayerId::PLAYER2]++;
+    int player_id_1 = (int)PlayerId::PLAYER1,
+        player_id_2 = (int)PlayerId::PLAYER2;
+    if (no_of_bots[player_id_1] == no_of_bots[player_id_2])
+        return;
+    int dominant_player_id = no_of_bots[player_id_1] > no_of_bots[player_id_2]
+                                 ? player_id_1
+                                 : player_id_2;
+    scores[dominant_player_id]++;
 }
 
 std::array<size_t, 2> ScoreManager::getScores() { return scores; }
