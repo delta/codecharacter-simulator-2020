ef13e292476e5906dbb4bbb34315556c5215ec06
Sai Hemanth <saihemanth9019@gmail.com>
2020-02-11 17:00:33 +0530

    Add player code wrapper

--- src/constants/include/constants/actor.h
+++ src/constants/include/constants/actor.h
@@ -17,13 +17,19 @@ const size_t MAX_NUM_BOTS = 500;
 // Number of bots that each player starts with
 const size_t NUM_BOTS_START = 20;
 
-// Blast impact radius around the bot (Manhattan distance)
-const size_t BLAST_IMPACT_RADIUS = 3;
+// Blast impact radius around the bot
+const size_t BOT_BLAST_IMPACT_RADIUS = 3;
 
 // Speed of a bot in manhattan distance per turn
 const int64_t BOT_SPEED = 2;
 
 // Frequency at which bots spawn at home base per turn
 const int64_t BOT_SPAWN_FREQUENCY = 1;
+
+// Maximum number of towers per player
+const size_t MAX_NUM_TOWERS = 200;
+
+// Blast impact radius around the tower
+const size_t TOWER_BLAST_IMPACT_RADIUS = 3;
 
 } // namespace Constants::Actor
