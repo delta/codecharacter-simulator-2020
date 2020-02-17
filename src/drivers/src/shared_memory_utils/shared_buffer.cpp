bc9f7bcaee4eb537a143edc4a720b2faf05927bd
shb9019 <saihemanth9019@gmail.com>
2020-02-17 20:46:52 +0530

    Add shared memory utils (#31)

---
+++ src/drivers/src/shared_memory_utils/shared_buffer.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file shared_buffer.cpp
+ * Definitions for buffer for shared memory
+ */
+
+#include "drivers/shared_memory_utils/shared_buffer.h"
+
+#include <utility>
+
+namespace drivers {
+
+SharedBuffer::SharedBuffer(bool is_player_running, int64_t instruction_counter,
+                           transfer_state::State transfer_state)
+    : is_player_running(is_player_running),
+      instruction_counter(instruction_counter),
+      transfer_state(std::move(transfer_state)) {}
+} // namespace drivers
