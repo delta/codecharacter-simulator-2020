41abd5b8b6fd7d8eda76965d7d09ef28ed0eb2b1
Sai Hemanth <saihemanth9019@gmail.com>
2020-02-21 21:58:52 +0530

    tmp

---
+++ src/player_code/src/player_code.cpp
@@ -0,0 +1,9 @@
+#include "player_code/player_code.h"
+
+namespace player_code {
+
+using namespace player_state;
+
+State PlayerCode::update(State state) { return state; }
+
+} // namespace player_code
