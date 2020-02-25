a56199d89b45f5771e74f09a190cd7826d05f40d
Hrishikesh Shekhar <hrishikeshshekhar@gmail.com>
2020-02-25 22:39:29 +0530

    Fix type in tower constructor

--- src/state/src/actor/tower.cpp
+++ src/state/src/actor/tower.cpp
@@ -19,7 +19,7 @@ Tower::Tower(ActorId id, PlayerId player_id, size_t hp, size_t max_hp,
 
 Tower::Tower(PlayerId player_id, size_t hp, size_t max_hp, DoubleVec2D position,
              size_t damage_points, size_t blast_range,
-             ScoreManager *score_manger, BlastCallback blast_callback)
+             ScoreManager *score_manager, BlastCallback blast_callback)
     : Actor(player_id, ActorType::TOWER, hp, max_hp, position, score_manager),
       Blaster(blast_range, damage_points, std::move(blast_callback)),
       state(std::make_unique<TowerIdleState>(this)), age(0) {}
