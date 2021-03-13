7b5390fff5584f0a053ef94fff44a19ef6ee49b3
Dipesh Kafle <38459660+dipeshkaphle@users.noreply.github.com>
2021-03-13 22:07:58 +0545

    Add workflows in CircleCI config (#74)
    - Add circleci config validation in pre-commit hook
    - Add ssh fingerprint for proto submodule
    - Update cURL for circleci pip installation
    - Set cmake-format package version in circleci config
    
    Co-authored-by: kumaran-14 <kumarangowrisankar@yahoo.in>

--- .circleci/cmake-format-check.sh
+++ .circleci/cmake-format-check.sh
@@ -8,20 +8,19 @@ cd /root/project
 # Run cmakeformat make target
 make cmakeformat
 
-
 #Check git for changes
 cd /root/project
-cmakeformatchecklist=`git status | grep "modified"`
+cmakeformatchecklist=$(git status | grep "modified")
 echo $cmakeformatchecklist
 if [[ -z $cmakeformatchecklist ]]; then
     echo "cmake-format test passed!"
-    exit 0;
+    exit 0
 else
-    echo "You have cmake-format problems in files :";
+    echo "You have cmake-format problems in files :"
     echo "$cmakeformatchecklist"
     for f in $cmakeformatchecklist; do
-        filename=`echo $f | awk '{print $2}'`
+        filename=$(echo $f | awk '{print $2}')
         echo "$(git diff $filename)"
     done
-    exit 1;
+    exit 1
 fi
